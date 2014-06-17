825627a321e76f43b84c44ea12cd7d2f5880f7ad
SESI-Phonegap <sesimx@gmail.com>
2014-06-17 18:36:52 -0500

    ok1231

--- js/main.js
+++ js/main.js
@@ -186,7 +186,7 @@ function handleStatusChange(response) {
 }
 
 function promptLogin() {
-  FB.login(null, {scope: 'public_profile,email,user_friends,publish_actions,user_place_visits'});
+  FB.login(null, {scope: 'public_profile,email,user_friends,publish_actions'});
 }
 
 function updateUserInfo(response) {
